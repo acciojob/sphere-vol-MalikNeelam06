69b024ebeac9551f3ad919f2b089f159095d0aaf
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-08-25 17:08:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+let radius = document.getElementById('radius');
+let volume = document.getElementById('volume');
+let vol;
 function volume_sphere() {
     //Write your code here
+	let r= radius.value;
+	vol=(4/3)*Math.pi*Math.pow(r,3);
+	volume.value=vol;
   
 } 
 
